6b382ce291631ea156ccfa3e7fa1e67fa38d3c4a
stesliuc <mihaitesliuc@gmail.com>
2024-05-20 18:17:19 -0400

    add nice formatting to projects page

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -73,7 +73,7 @@ export function NavigationMenuHeader() {
                 <NavigationMenuLink asChild>
                   <a
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
+                    href="/projects"
                   >
                     <div className="mb-2 mt-4 text-lg font-medium">
                       Projects
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,14 +3,26 @@
 export default function Projects() {
     return (
       <main>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Projects
+        </h1>
         <div id="scraper">
-          Scraper
+            <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+            Scraper
+            </h2>
+            <p>
+                lorem ipsum
+            </p>
         </div>
         <div id="portfolio">
-          Portfolio
+        <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+            Portfolio
+        </h2>
         </div>
         <div id = "llm">
-          LLM
+        <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+            Llama3 Model
+        </h2>
         </div>
       </main>
     );
