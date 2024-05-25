e36451042d3fb3ded918d8a3680ca5720afe5a1f
stesliuc <mihaitesliuc@gmail.com>
2024-05-25 10:48:26 -0400

    add icons to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,56 @@
-//src/app/components/header.tsx
+//src/components/header.tsx
 import Link from "next/link";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
 import { NavigationMenuHeader } from "./navbar"
 import { Logo } from "./logo"
+import { Icons } from "./icons";
+import { buttonVariants } from "./ui/button";
 
 const Header = () => {
   return (
     <>
-        <div className="sticky bg-background p-4 margin-top-0 w-full sticky top-0">
-          <div className="flex flex:1 flex-row justify-between">
-            <Logo />
-            <NavigationMenuHeader />
-          </div>
+      <div className="z-10 sticky bg-background py-4 w-full top-0">
+        <div className="container flex max-w-screen-2xl items-center">
+          <Logo />
+          <NavigationMenuHeader />
+          <div className="flex flex-1 items-center justify-end space-x-2">
+          <nav className="flex items-center">
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={cn(
+                  buttonVariants({ variant: "ghost" }),
+                  "w-10 px-0 hidden sm:inline-flex"
+                )}
+              >
+                <Icons.gitHub className="h-4 w-4" />
+                <span className="sr-only">GitHub</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.linkedin}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={cn(
+                  buttonVariants({ variant: "ghost" }),
+                  "w-10 px-0 hidden sm:inline-flex"
+                )}
+              >
+                <Icons.linkedin className="h-4 w-4" />
+                <span className="sr-only">Linkedin</span>
+              </div>
+            </Link>
+          </nav>
         </div>
+        </div>
+
+      </div>
     </>
   );
 };
