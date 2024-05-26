7b54ec34275af01cdb390f41019317eb03704ec7
stesliuc <mihaitesliuc@gmail.com>
2024-05-25 23:03:39 -0400

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,27 @@
 //src/app/components/footer.tsx
 
+import { siteConfig } from "@/config/site";
+import { Mail } from "lucide-react";
+import { Icons } from "./icons";
+
 const Footer = () => {
     return (
-        <>
-            <footer className="bg-background text-center text-xs margin-bottom-0 p-4">
-                Made by Stefan Tesliuc
-            </footer>
-        </>
+        <footer>
+            <div className="mb-6 mt-14 flex flex-col items-center">
+                <div className="mb-3 flex space-x-4">
+                    <a target="_blank" rel="noreferrer" href="mihaitesliuc@gmail.com">
+                        <span className="sr-only">Mail</span>
+                        <Mail className="h-5 w-5" />
+                    </a>
+                    <a target="_blank" rel="noreferrer" href={siteConfig.links.github}>
+                        <span className="sr-only">GitHub</span>
+                        <Icons.gitHub className="h-5 w-5" />
+                    </a>
+                    <p className="text-sm">Made by Stefan Tesliuc</p>
+                </div>
+            </div>
+        </footer>
+        
     )
 }
 
