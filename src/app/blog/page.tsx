c4c7a1b84b7909f3f9488b4600b6ce985d3b5eb8
stesliuc <mihaitesliuc@gmail.com>
2024-05-27 11:39:44 -0400

    display tags on blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,8 @@
 import { posts } from "#site/content";
 import { PostItem } from "@/components/post-item";
 import { QueryPagination } from "@/components/query-pagination";
-import { sortPosts } from "@/lib/utils";
+import { getAllTags, sortPosts, sortTagsByCount } from "@/lib/utils";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Metadata } from "next";
 import { Tag } from "@/components/tag";
 
@@ -30,21 +31,26 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
     POSTS_PER_PAGE * currentPage
   );
 
+  const tags = getAllTags(posts);
+  const sortedTags = sortTagsByCount(tags);
+
   return (
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">
             Blog
           </h1>
-          <p className="leading-7 [&:not(:first-child)]:text-xl text-muted-foreground ">
+          <p className="text-xl text-muted-foreground">
             My disjointed thoughts on science, philosophy, and everything currently interesting to me.
           </p>
-        </div>
+          </div>
       </div>
-      <hr className="mt-8" />
-      {displayPosts?.length > 0 ? (
-        <ul className="flex flex-col">
+      <div className="grid grid-cols-12 gap-3 mt-8">
+        <div className="col-span-12 col-start-1 sm:col-span-8">
+          <hr />
+          {displayPosts?.length > 0 ? (
+            <ul className="flex flex-col">
               {displayPosts.map((post) => {
                 const { slug, date, title, description, tags } = post;
                 return (
@@ -59,11 +65,26 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
                   </li>
                 );
               })}
-        </ul>
-      ) : (
-        <p>nothing to see here yet</p>
-      )}
-      <QueryPagination totalPages={totalPages} className="justify-end mt-4"/>
-     </div>
+            </ul>
+          ) : (
+            <p>Nothing to see here yet</p>
+          )}
+          <QueryPagination
+            totalPages={totalPages}
+            className="justify-end mt-4"
+          />
+        </div>
+        <Card className="col-span-12 row-start-3 h-fit sm:col-span-4 sm:col-start-9 sm:row-start-1">
+          <CardHeader>
+            <CardTitle>Tags</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-wrap gap-2">
+            {sortedTags?.map((tag) => (
+              <Tag tag={tag} key={tag} count={tags[tag]} />
+            ))}
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   );
 }
