0507dca8ebde4342382c0b8be6fe557eb9b83fc4
stesliuc <mihaitesliuc@gmail.com>
2024-06-01 19:21:08 -0400

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 import { Providers } from "@/components/providers";
 import { siteConfig } from "@/config/site";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans"});
 
