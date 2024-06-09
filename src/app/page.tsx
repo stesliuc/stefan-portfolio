dc00b6e799f81d374b652727eee595a7a2e046c0
stesliuc <mihaitesliuc@gmail.com>
2024-06-09 18:43:11 -0400

    about me updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,13 +33,12 @@ export default function Home() {
             <p>Ursula K. Le Guin</p>
           </blockquote>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
-            I am interested in the intersection of knowledge domains, specifically how insights in the natural sciences, psychology, neuroscience, and philosophy can create breakthroughs in math and computer science.
+            I am interested in how neurosicence, psychology, and philosophy can be applied to make machine learning algorithms smarter.
           </p>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
-            Category theory tackled relational problems in consciouness, 
-            thermodynamics gave rise to stable diffusion alogorithms, 
-            and entymology spawned ACO algorithms from patterns found in ant behavior.
-            I believe in the study of the patterns and threads that bind ideas together. 
+            I feel like we have skipped a few steps with Artificial Intelligence, the first being an understanding of what intelligence actually is.
+            Instead of trying to improve on whatever iteration of GPT we are currently on, it makes more sense to reground our development of ML algorithms from the principles of how the human brain works.
+            Of course we don&apos;t know how it works, but I am excited that a deeper study of AI can naturally lead to a deeper study and appreciation of how we function.
           </p>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
           </p>
@@ -48,9 +47,11 @@ export default function Home() {
           </h3>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
             I am most interested in developing machines that can think and feel and understand. 
-            A conscious computer is predicated by an algorithmic understanding of subjective experience, 
-            and thus is more suited to making and comprehending ethical decisions.
-            If this goal is even possible, it is through a joint effort in neuroscience, computer science, quantum phyiscs, and philosophy.
+            Sounds easy! 
+            Don&apos;t worry, I&apos;m not some doomer who believes in ushering in the next way of supreme AI overlords, 
+            I think developing machine consciousness is the biggest thrust we can undertake to make algorithms and AI systems that are more human and humane
+            A conscious computer would by definition understand subjective experience, and thus be more suited to making ethical decisions.
+            This seems preferable to unleashing some binary task masters and then have researchers scrambling to teach it to be fair after the fact.
           </p>
           <h3 className="mt-8 scroll-m-20 text-2xl pt-3 font-semibold tracking-tight">
             AI Fairness
@@ -58,22 +59,19 @@ export default function Home() {
           <p className="leading-7 [&:not(:first-child)]:mt-6">
             Our current methods of determining alogirthmic fairness are grounded in quantitative data analysis, 
             which will never understand the depths of the human experience. 
-            Life is personal and complex and can never be distilled to a few sets of rules, 
-            meaning rules-based AI systems are fundamentally anti-human in design.
+            Life is too complex to be distilled to a few sets of rules, meaning rules-based AI systems are anti-human in design.
           </p>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
-            I am interested in developing AI that can understand the impacts of its decisions and can learn to act fairly, 
-            with consideration of every outlier.
+            I am interested in AI that can understand the impacts of its decisions and can learn to act fairly.
           </p>
           <h3 className="mt-8 scroll-m-20 text-2xl pt-3 font-semibold tracking-tight">
             ML in Banking
           </h3>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
-            I am currently working in the financial industry, which is an exciting frontier for ML at the heart of many people&apos;s lives.
+            I am currently working in the financial industry, which is an application of ML at the heart of many people&apos;s lives.
           </p>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
-            I am currently working on utilizing embeddings of transaction data as direct features to perform better customer segmentation,
-            and am involved in the development of foundation models that can leverage customer inputs to deliver tailored financial services.
+            I am currently working on utilizing foundation models to understand the financial life cycle of customers.
           </p>
         </div>
       </div>
